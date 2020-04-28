8f1c2189c642b5d49a049c2d66b49f28ee442163
makw <ma_kewen@foxmail.com>
2020-04-28 10:37:10 +0800

    1.上传SQL脚本 2.库名修改为itheima_spring，对应修改jdbc-url

--- day01_eesy_01jdbc/src/main/java/com/iteima/jdbc/JdbcDemo1.java
+++ day01_eesy_01jdbc/src/main/java/com/iteima/jdbc/JdbcDemo1.java
@@ -27,15 +27,14 @@ public class JdbcDemo1 {
         Class.forName("com.mysql.jdbc.Driver");
 
         //2.获取连接
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mybatis","root","root");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/itheima_spring","root","root");
         //3.获取操作数据库的预处理对象
         PreparedStatement pstm = conn.prepareStatement("select * from account");
         //4.执行SQL，得到结果集
         ResultSet rs = pstm.executeQuery();
         //5.遍历结果集
         while(rs.next()){
-//            System.out.println(rs.getString("name"));
-            System.out.println(rs.getString("money"));
+            System.out.println(rs.getString("name"));
         }
         //6.释放资源
         rs.close();
